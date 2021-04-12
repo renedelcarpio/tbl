61ed7987b98fb2f864eeeb7d5a2d4a118383eee4
Rene Del Carpio <delcarpio.rene@gmail.com>
2021-04-12 13:12:09 -0400

    archivos creados para el desarrollo del navbar

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter, ReactRouter, Route, Switch } from 'react-router-dom'
+
+import Layout from './Layout'
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Layout>
+                <Switch>
+                    
+                </Switch>
+            </Layout>
+        </BrowserRouter>
+    )
+}
+export default App
---
+++ src/components/MenuItems.js
@@ -0,0 +1,22 @@
+ export const MenuItems = [
+    {
+        title: 'Nosotros',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Ejemplares',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Servicios',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contacto',
+        url: '#',
+        cName: 'nav-links'
+    },
+]
