561a78b2b1cf3096b6cf1d4ee41cc14c199c6519
Rene Del Carpio <delcarpio.rene@gmail.com>
2021-04-14 14:58:12 -0400

    Footer has been add

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import '../styles/Footer.css'
+function Footer() {
+    return (
+        <>
+            <div className='footer__container'>
+                <h5>© Thunderbolt-Labs. El contendio de esta página web no es de dominio público. Todos los derechos reservados.</h5>
+            </div>
+        </>
+    )
+}
+
+export default Footer
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import Navbar from './Navbar'
+import Footer from './Footer'
 
 function Layout(props) {
     return (
         <>
             <Navbar/>
             { props.children}
+            <Footer/>
         </>
     )
 }
