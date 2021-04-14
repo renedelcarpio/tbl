162174a68f4d1b93986d6cac413abafa80a772c6
Rene Del Carpio <delcarpio.rene@gmail.com>
2021-04-14 18:14:21 -0400

    Dogs pages was add, there were corrections in Home.css and Navbar.css

--- src/pages/Dogs.js
+++ src/pages/Dogs.js
@@ -1,16 +1,54 @@
 import React from 'react'
 
+import '../styles/Dogs.css'
 import labrador1 from '../images/labrador.jpg'
 
 function Dogs() {
     return (
         <>
-            <div className='badge__main-container' >
-                <img className='badge__image-container' src={ labrador1 } alt=""/>
-                    <h2 className='badge__title'>Nombre del perro</h2>
+            <div className='badge__main-container'>
+                <div className='badge__image-container' >
+                    <img className='badge__image' src={ labrador1 } alt="Foto de un labrador"/>
+                </div>
+                <div className='badge__description-container'>
+                    <h1 className='badge__title'>Nombre del perro</h1>
                     <h3 className='badge__pedigree'>Pedigree</h3>
                     <p className='badge__about-dog'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui repellendus voluptatum commodi dolorem, magni enim quia ab quod itaque eum explicabo similique eius, architecto veritatis accusantium corporis. Asperiores, explicabo obcaecati?
                     </p>
+                </div>
+            </div>
+            <div className='badge__main-container'>
+                <div className='badge__image-container' >
+                    <img className='badge__image' src={ labrador1 } alt="Foto de un labrador"/>
+                </div>
+                <div className='badge__description-container'>
+                    <h1 className='badge__title'>Nombre del perro</h1>
+                    <h3 className='badge__pedigree'>Pedigree</h3>
+                    <p className='badge__about-dog'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui repellendus voluptatum commodi dolorem, magni enim quia ab quod itaque eum explicabo similique eius, architecto veritatis accusantium corporis. Asperiores, explicabo obcaecati?
+                    </p>
+                </div>
+            </div>
+            <div className='badge__main-container'>
+                <div className='badge__image-container' >
+                    <img className='badge__image' src={ labrador1 } alt="Foto de un labrador"/>
+                </div>
+                <div className='badge__description-container'>
+                    <h1 className='badge__title'>Nombre del perro</h1>
+                    <h3 className='badge__pedigree'>Pedigree</h3>
+                    <p className='badge__about-dog'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui repellendus voluptatum commodi dolorem, magni enim quia ab quod itaque eum explicabo similique eius, architecto veritatis accusantium corporis. Asperiores, explicabo obcaecati?
+                    </p>
+                </div>
+            </div>
+            <div className='badge__main-container'>
+                <div className='badge__image-container' >
+                    <img className='badge__image' src={ labrador1 } alt="Foto de un labrador"/>
+                </div>
+                <div className='badge__description-container'>
+                    <h1 className='badge__title'>Nombre del perro</h1>
+                    <h3 className='badge__pedigree'>Pedigree</h3>
+                    <p className='badge__about-dog'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui repellendus voluptatum commodi dolorem, magni enim quia ab quod itaque eum explicabo similique eius, architecto veritatis accusantium corporis. Asperiores, explicabo obcaecati?
+                    </p>
+                </div>
             </div>
         </>
     )
