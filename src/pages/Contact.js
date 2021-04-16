5abe00222c11b35e2bb72d66bcad1254900c429c
Rene Del Carpio <delcarpio.rene@gmail.com>
2021-04-16 17:27:25 -0400

    Starting de contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
+import Map from '../components/Map'
+import '../styles/Contact.css'
+
 function Contact() {
     return (
         <>
+            <div className='main__map-container'>
+                <Map/>
+            </div>
             <div>
-                <h1>Contacto</h1>
+                <h1>Formulario y redes sociales</h1>
             </div>
         </>
     )
