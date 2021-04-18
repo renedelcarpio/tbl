2f142cf96c68ad2e7d800f68510936ba6a755a3c
Rene Del Carpio <delcarpio.rene@gmail.com>
2021-04-18 16:36:19 -0400

    stiles for homepage were improve

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -5,19 +5,24 @@ import labrador2 from '../images/labrador2.jpg'
 
 function AboutUs() {
     return (
-        <>
-            <div className='about__main-container'>
-                <div className='about__image-container' >
-                    <img className='about__image' src={ labrador2 } alt="Foto de un labrador"/>
-                </div>
-                <div className='about__description-container'>
-                    <h1 className='about__title'>Thunderbolt-labs</h1>
-                    <h3 className='about__pedigree'>Quienes somos</h3>
-                    <p className='about__about-dog'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui repellendus voluptatum commodi dolorem, magni enim quia ab quod itaque eum explicabo similique eius, architecto veritatis accusantium corporis. Asperiores, explicabo obcaecati?
-                    </p>
-                </div>
+        <div className='about__main-container'>
+            <div className='about__image-container' >
+                <img className='about__image' src={ labrador2 } alt="Foto de un labrador"/>
             </div>
-        </>
+            <div className='about__description-container'>
+                <h1 className='about__title'>Thunderbolt-labs</h1>
+                <h3 className='about__pedigree'>Quienes somos</h3>
+                <p className='about__about-dog'>
+                    Nuestra familia lleva más de cinco años criando esta maravillosa raza y compartiendo con ellos muchas alegrías, tanto en competencias como sabiendo que son pilares en sus hogares, donde son cuidados con mucho amor.
+                </p>
+                <p className='about__about-dog'>
+                    Un criador no es algien que tiene 60 perros en un lugar sin espacio, criando en todos los celos, es un delincuente, no un criador.
+                </p>
+                <p className='about__about-dog'>
+                    La realidad es que un buen criador se preocupa por la salud y bienestar de sus perros. Un criador busca la familia idónea para conformar un hogar que les brinde seguridad y al mismo tiempo compartar una vida de alegría. Un criador siempre estará pendiente de cada cachorro o adulto así esté en otro hogar para tener la seguridad que es bien cuidado.
+                </p>
+            </div>
+        </div>
     )
 }
 
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import Map from '../components/Map'
 import '../styles/Contact.css'
@@ -11,6 +12,12 @@ function Contact() {
             </div>
             <div className='social-media-container'>
                 <h1>Síguenos en nuestras redes sociales:</h1>
+                <ul>
+                    <li><a href='https://www.facebook.com/search/top?q=Thunderbolt-Labs'><i className='fab fa-facebook-square'></i></a></li>
+                    <li><a href='https://www.instagram.com/thunderbolt_labs/?hl=es-la'><i className="fab fa-instagram-square"></i></a></li>
+                    <li>Email: thunderbolt-labs@gmail.com</li>
+                    <li>Teléfonos: (+591) 713 56163 / (+591) 709 38732</li>
+                </ul>
             </div>
         </>
     )
