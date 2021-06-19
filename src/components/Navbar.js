7a1484cc5063b6607fe0ea5192913ba91cdfef82
Rene Del Carpio <delcarpio.rene@gmail.com>
2021-06-18 21:10:45 -0400

    se corrigio algunos estilos en Home y se cambio el componente tipo clase de Navbar a un componente funcional utilizando hooks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,45 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-import '../styles/Navbar.css'
-import { MenuItems } from './MenuItems'
-import tblLogo from '../images/tbl_sin_fondo.png'
+import '../styles/Navbar.css';
+import { MenuItems } from './MenuItems';
+import tblLogo from '../images/tbl_sin_fondo.png';
 
-class Navbar extends React.Component {
-    state = { clicked: false }
+const Navbar = () => {
+	const [openMenu, setOpenMenu] = useState(false);
 
-    handleClick = () => {
-        this.setState({ clicked: !this.state.clicked})
-    }
+	const handleClick = () => {
+		setOpenMenu(!openMenu);
+	};
 
-    render() {
-        return(
-            <>
-                <nav className='NavbarItems'>
-                    <img className='navbar-img' src={ tblLogo } alt=""/>
-                    <h1 className='navbar-logo'><Link className='navbar-logo navbar-logo-link' to="/">Thunderbolt-Labs</Link></h1>
-                    <div className='menu-icon' onClick={ this.handleClick }>
-                        <i className={ this.state.clicked ? 'fas fa-times' : 'fas fa-bars' }></i>
-                    </div>
-                    <ul className={ this.state.clicked ? 'nav-menu active' : 'nav-menu' }>
-                        { MenuItems.map((item, index) => {
-                            return(
-                                <li key={ index }>
-                                    <Link className={ item.cName } to={ item.url }>
-                                        { item.title }
-                                    </Link>
-                                </li>
-                            )
-                        }) }
-                    </ul>
-                </nav>
-            </>
-        )
-    }
-}
+	return (
+		<>
+			<nav className='NavbarItems'>
+				<img className='navbar-img' src={tblLogo} alt='' />
+				<h1 className='navbar-logo'>
+					<Link className='navbar-logo navbar-logo-link' to='/'>
+						Thunderbolt-Labs
+					</Link>
+				</h1>
+				<div className='menu-icon' onClick={handleClick}>
+					<i
+						className={openMenu === false ? 'fas fa-times' : 'fas fa-bars'}
+					></i>
+				</div>
+				<ul className={openMenu === true ? 'nav-menu active' : 'nav-menu'}>
+					{MenuItems.map((item, index) => {
+						return (
+							<li key={index}>
+								<Link className={item.cName} to={item.url}>
+									{item.title}
+								</Link>
+							</li>
+						);
+					})}
+				</ul>
+			</nav>
+		</>
+	);
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,24 +6,34 @@ import tallImage from '../images/labrador.jpg';
 const ImageContainer = styled.div`
 	display: flex;
 	justify-content: center;
+	align-items: center;
 	margin: auto;
 	margin-top: 3rem;
 	margin-bottom: 3rem;
 	border-radius: 10px;
 	width: 80%;
-	max-height: 80%;
+	max-height: 90rem;
 	overflow: hidden;
+	box-shadow: 0.3rem 0.5rem 0.5rem #888888;
+
+	@media (max-width: 1025px) {
+		margin: auto;
+	}
 
 	@media (max-width: 800px) {
 		max-width: 60rem;
 	}
 
 	@media (max-width: 450px) {
+		margin-top: 3rem;
+		margin-bottom: 3rem;
 		max-width: 40rem;
+		max-height: 69rem;
 	}
 `;
 
 const MainImage = styled.img`
+	width: 100%;
 	border-radius: 10px;
 `;
 
